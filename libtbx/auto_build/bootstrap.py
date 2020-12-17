11db9a55acbcd271a65b575bd3f2db0e699834a5
Russ Taylor <russ@reliasolve.com>
2020-12-17 15:59:22 -0500

    Pulls a version of Reduce that wraps itself as cctbx_reduce_ext and provides a reduce.py script that loads it that way to test.

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -988,8 +988,8 @@ class suitename_module(SourceModule):
 
 class reduce_module(SourceModule):
   module = 'reduce'
-  anonymous = ['git', '-b v3.9.2',
-               'https://github.com/rlabduke/reduce.git']
+  anonymous = ['git', '-b develop',
+               'https://github.com/reliasolve/reduce.git']
 
 class king_module(SourceModule):
   module = 'king'
